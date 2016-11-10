00211893406f469bec77cbfe7defb0b5bcdb0895
iLia Lumpov <i@lumpov.ru>
2016-11-10 12:05:30 +0400

    попытка вылечить segfault 2

--- tests/cyberplat.js
+++ tests/cyberplat.js
@@ -21,11 +21,11 @@ typedef struct
 }IPRIV_KEY;
 */
 var structIPRIV_KEY = new struct({
-  'eng': 'short',
-  'type': 'short',
-  'keyserial': 'ulong',
-  'userid': 'char',
-  'key': 'short'
+  'eng': 'short', //2
+  'type': 'short', //2
+  'keyserial': 'ulong', //4
+  'userid': 'char', //24
+  'key': 'short' //2
 });
 
 var reftypeIPRIV_KEY = ref.refType(structIPRIV_KEY);
@@ -33,9 +33,9 @@ var reftypeIPRIV_KEY = ref.refType(structIPRIV_KEY);
 var libipriv = ffi.Library('./libipriv', {
   'Crypt_Initialize':  [ 'int', [ ] ],
   //int IPRIVAPI Crypt_OpenSecretKeyFromFile(int eng,const char* path,const char* passwd,IPRIV_KEY* key);
-  'Crypt_OpenSecretKeyFromFile': [ 'int', [ 'int', 'string', 'string', 'char *' ] ],
+  'Crypt_OpenSecretKeyFromFile': [ 'int', [ 'int', 'char *', 'char *', 'char *' ] ],
   //int IPRIVAPI Crypt_OpenPublicKeyFromFile(int eng,const char* path,unsigned long keyserial,IPRIV_KEY* key,IPRIV_KEY* cakey);
-  'Crypt_OpenPublicKeyFromFile': [ 'int', [ 'int', 'string', 'ulong', 'char *', 'char *' ] ],
+  'Crypt_OpenPublicKeyFromFile': [ 'int', [ 'int', 'char *', 'ulong', 'char *', 'char *' ] ],
   //int IPRIVAPI Crypt_Sign(const char* src,int nsrc,char* dst,int ndst,IPRIV_KEY* key);
   'Crypt_Sign': [ 'int', [ 'char *', 'int', 'char *', 'int', 'char *' ] ],
   //int IPRIVAPI Crypt_Verify(const char* src,int nsrc,const char** pdst,int* pndst,IPRIV_KEY* key);
@@ -59,21 +59,32 @@ srcbuffer.write('hello world!')
 const buffer = new Buffer(1024);
 buffer.fill(0);
 
-const ptrIPrivKey = new Buffer(36);
+const ptrIPrivKey = new Buffer(34);
 ptrIPrivKey.fill(0);
-const ptrIPubKey1 = new Buffer(36);
+const ptrIPubKey1 = new Buffer(34);
 ptrIPubKey1.fill(0);
-const ptrIPubKey2 = new Buffer(36);
+const ptrIPubKey2 = new Buffer(34);
 ptrIPubKey2.fill(0);
 
+const bufSecretKeyPath = new Buffer(36);
+bufSecretKeyPath.fill(0);
+bufSecretKeyPath.write('./secret.key');
+
+const bufPassword = new Buffer(36);
+bufPassword.fill(0);
+bufPassword.write('1111111111');
+
+const bufPublicKeyPath = new Buffer(36);
+bufPublicKeyPath.fill(0);
+bufPublicKeyPath.write('./pubkeys.key');
 
 //rc=Crypt_OpenSecretKeyFromFile(eng,"secret.key","1111111111",&sec);			// Загрузка собственного закрытого ключа
-rc = libipriv.Crypt_OpenSecretKeyFromFile(eng, './secret.key', '1111111111', ptrIPrivKey);
+rc = libipriv.Crypt_OpenSecretKeyFromFile(eng, bufSecretKeyPath, bufPassword, ptrIPrivKey);
 console.log('Crypt_OpenSecretKeyFromFile='+rc);
 console.log('ptrIPrivKey='+JSON.stringify(ptrIPrivKey));
 
 //rc=Crypt_OpenPublicKeyFromFile(eng,"pubkeys.key",17033,&pub1,0);		// Загрузка собственного открытого ключа
-rc = libipriv.Crypt_OpenPublicKeyFromFile(eng, './pubkeys.key', '17033', ptrIPubKey1, null);
+rc = libipriv.Crypt_OpenPublicKeyFromFile(eng, bufPublicKeyPath, 17033, ptrIPubKey1, null);
 console.log('Crypt_OpenPublicKeyFromFile='+rc);
 console.log('ptrIPubKey1='+JSON.stringify(ptrIPubKey1));
 
