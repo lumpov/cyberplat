7b579da4f5addef962e04aedbee6e866e232b8cf
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-12-10 23:12:24 +0700

    write cyberplat first usage example

--- tests/cyberplat-test.js
+++ tests/cyberplat-test.js
@@ -1,10 +1,10 @@
 var assert = require('assert');
-var CyberplatAgent = require('../index')
+var Cyberplat = require('../index')
 
 describe("Cyberplat", function() {
     
-    it("check create", function() {
-        var cyberplatAgent = new CyberplatAgent({
+    it("check create", function(done) {
+        var cyberplat = new Cyberplat({
             crypto: {
                 debug: true,
                 libPath: './tests/libipriv',
@@ -16,6 +16,20 @@ describe("Cyberplat", function() {
                 SD: 2,
                 OP: 3
             }
+        });
+
+        var obj = {
+            DATE: "12",
+            SESSION: 'session',
+            AMOUNT: "1.00",
+            AMOUNT_ALL: "1.00",
+            TERM_ID: "1",
+            NUMBER: "1212"
+        };
+
+        cyberplat.payCheck(obj, function(response) {
+            console.log(response.body);
+            done();
         });
 
     }); 
