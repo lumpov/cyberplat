4dce7002dd680c99ceafdad0ae9dfc63059ea020
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-12-10 22:57:43 +0700

    add methods to cyberplat

--- index.js
+++ index.js
@@ -13,14 +13,34 @@ var Cyberplat = function (ops) {
         var message = builder.buildMessage(type, obj);
         var signedMessage = crypto.sign(message);
         client.request(type, signedMessage, callback);
-    }
+    };
 
     var payCheck = function (obj, callback) {
         go('payCheck', obj, callback);
     };
 
-    return {
+    var pay = function (obj, callback) {
+        go('pay', obj, callback);
+    };
 
+    var payStatus = function (obj, callback) {
+        go('payStatus', obj, callback);
+    };
+
+    var limitStatus = function (obj, callback) {
+        go('limitStatus', obj, callback);
+    };
+
+    var fillStatus = function (obj, callback) {
+        go('fillStatus', obj, callback);
+    };
+
+    return {
+        payCheck: payCheck,
+        pay: pay,
+        payStatus: payStatus,
+        limitStatus: limitStatus,
+        fillStatus: fillStatus
     };
 };
 
